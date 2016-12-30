6a3f0406b3f9e69d2300e131a9ac1c95e70604d8
Laurent Debacker <debackerl@gmail.com>
2016-12-30 19:33:30 +0100

    Fix direct resolve, and make more eslint compliant
    Previously, doing "new Dodgy(function(resolve, reject) { resolve(); })" would fail because it would try to assign "status" to undefined because dog wasn't assigned yet.
    
    Also, eslint was complaining that evolved and abort were being used before definition.

--- src/dodgy.js
+++ src/dodgy.js
@@ -1,38 +1,3 @@
-function Dodgy(callback, resolvable) {
-  var
-    resolve, reject, abort,
-    status = 'pending',
-    dog = new Promise(function (res, rej) {
-      callback(
-        resolve = function (value) {
-          if (status === 'pending') {
-            status = 'resolved';
-            dog.status = status;
-            res(value);
-          }
-        },
-        reject = function (value) {
-          if (status === 'pending') {
-            status = 'rejected';
-            dog.status = status;
-            rej(value);
-          }
-        },
-        function onAbort(callback) {
-          abort = function (reason) {
-            if (status === 'pending') {
-              status = 'aborted';
-              dog.status = status;
-              rej(callback(reason));
-            }
-          };
-        }
-      );
-    })
-  ;
-  return evolved(dog, resolvable, abort, resolve, reject);
-}
-
 function evolved(dog, resolvable, abort, resolve, reject) {
   var
     currentThen = dog.then,
@@ -58,14 +23,52 @@ function evolved(dog, resolvable, abort, resolve, reject) {
   return dog;
 }
 
+function Dodgy(callback, resolvable) {
+  var
+    resolve, reject, abort,
+    status = 'pending',
+    _res, _rej,
+    dog = new Promise(function (res, rej) {
+      _res = res;
+      _rej = rej;
+    })
+  ;
+  callback(
+    resolve = function (value) {
+      if (status === 'pending') {
+        status = 'resolved';
+        dog.status = status;
+        _res(value);
+      }
+    },
+    reject = function (value) {
+      if (status === 'pending') {
+        status = 'rejected';
+        dog.status = status;
+        _rej(value);
+      }
+    },
+    function onAbort(callback) {
+      abort = function (reason) {
+        if (status === 'pending') {
+          status = 'aborted';
+          dog.status = status;
+          _rej(callback(reason));
+        }
+      };
+    }
+  );
+  return evolved(dog, resolvable, abort, resolve, reject);
+}
+
 Dodgy.race = function (iterable) {
-  var dog = Promise.race(iterable).then(abort);
   function abort(result) {
     for (var i = 0; i < iterable.length; i++) {
       if ('abort' in iterable[i]) iterable[i].abort();
     }
     return result;
   }
+  var dog = Promise.race(iterable).then(abort);
   dog.abort = abort;
   return dog;
 };
